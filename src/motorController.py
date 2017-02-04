527794c5f059adcb32ccbffed54cc3e8b6bf6f2f
Andrew <Andy.keech@live.ca>
2017-02-04 03:14:03 -0500

    added motor functions

--- src/motorController.py
+++ src/motorController.py
@@ -1,24 +1,37 @@
+import src.stepperTest2 as motor
+
+#Order is Red, Blue, Green.
+
+redDistance = 200;
+greenDistance=200;
+dumpDistance=600;
+right=2
+left=-2
+
 def dumpRed():
     print("red")
     # move to red
+    motor.move(left,redDistance,"bottom")
     dump()
     # moveback
+    motor.move(right, redDistance, "bottom")
 
 def dumpGreen():
     print("green")
-    # move to green
+    motor.move(right,greenDistance,"bottom")
     dump()
     # moveback
+    motor.move(left, greenDistance, "bottom")
 
 def dumpBlue():
     print("Blue")
-    #move to blue
     dump()
-    #moveback
 
 def dump():
     print("dumping")
     #dump piece
+    motor.move(2, dumpDistance, "top")
+    motor.move(-2, dumpDistance, "top")
     #reset
 
 def dropTo(colour):
@@ -28,3 +41,6 @@ def dropTo(colour):
         dumpGreen()
     elif (colour == 'blue'):
         dumpBlue()
+
+
+dropTo('red')
---
+++ src/stepperTest2.py
@@ -0,0 +1,65 @@
+import sys
+import time
+import RPi.GPIO as GPIO
+
+def move(direction, distance, motor):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+
+    if (motor == "bottom"):
+        StepPins = [3,5,7,11]
+    else:
+        StepPins = [31,33,35,37]
+    
+    for pin in StepPins:
+        print("pin setup of " + str(pin))
+        GPIO.setup(pin,GPIO.OUT)
+        GPIO.output(pin, False)
+
+    Seq = [[1,0,0,1],
+           [1,0,0,0],
+           [1,1,0,0],
+           [0,1,0,0],
+           [0,1,1,0],
+           [0,0,1,0],
+           [0,0,1,1],
+           [0,0,0,1]]   
+    StepCount = len(Seq)
+ 
+    # Initialise variables
+    StepCounter = 0
+    StepDir = direction
+    duration = distance
+ 
+    # Start main loop
+    for x in range(1,duration):
+ 
+      for pin in range(0,4):
+        xpin=StepPins[pin]# Get GPIO
+        if Seq[StepCounter][pin]!=0:
+          GPIO.output(xpin, True)
+        else:
+          GPIO.output(xpin, False)
+ 
+      StepCounter += StepDir
+ 
+      # If we reach the end of the sequence
+      # start again
+      if (StepCounter>=StepCount):
+        StepCounter = 0
+      if (StepCounter<0):
+        StepCounter = StepCount+StepDir
+ 
+      # Wait before moving on
+      time.sleep(.003)
+
+    # Initialise variables
+    StepDir = -2 # Set to 1 or 2 for clockwise
+            # Set to -1 or -2 for anti-clockwise
+    StepCounter = 0
+
+move(-2,200,"bottom")
+move(2,600,"top")
+move(-2,600,"top")
+move(2,200,"bottom")
+
